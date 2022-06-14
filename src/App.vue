79e715dbb129b35d52b102562bd99ddd5a65b114
Emily Blakseth <ekblakse@stud.ntnu.no>
2022-06-15 00:40:27 +0200

    add footer, button for contact, style for h1 and update bg color

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <v-app>
-    <v-app-bar color="green">
-      <v-btn @click="router.push('/')">Home</v-btn>
+  <v-app class="bg-pink-lighten-4">
+    <v-app-bar color="pink"
+      ><v-spacer />
+      <v-btn @click="router.push('/')">Hjem</v-btn>
       <v-btn @click="router.push('experience')">Erfaring</v-btn>
-      <v-btn @click="router.push('projects')">Prosjekter</v-btn> </v-app-bar
+      <v-btn @click="router.push('projects')">Prosjekter</v-btn>
+      <v-btn @click="router.push('contact')">Kontakt meg</v-btn></v-app-bar
     ><v-main> <router-view /> </v-main>
+    <v-footer class="bg-pink-lighten-4 text-center"
+      ><v-spacer /> Juli 2022 <v-spacer />
+    </v-footer>
   </v-app>
 </template>
 
@@ -15,3 +20,13 @@ import { useRouter } from "vue-router";
 //gir metoden et navn
 const router = useRouter();
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Anton&family=Fascinate&family=Kdam+Thmor+Pro&display=swap");
+
+h1 {
+  color: #30051b;
+  font-size: 50px;
+  font-family: "Anton";
+}
+</style>
