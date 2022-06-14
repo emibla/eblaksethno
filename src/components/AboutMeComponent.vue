0673e28268bea2330ebe5f17a795fdf9929a176c
Emily Blakseth <ekblakse@stud.ntnu.no>
2022-06-15 00:48:35 +0200

    Add Content to AboutMeComponent

--- src/components/AboutMeComponent.vue
+++ src/components/AboutMeComponent.vue
@@ -1,9 +1,23 @@
-<template>Heter Emily, kommer fra Molde</template>
+<template>
+  <v-container class="mt-10 w-75 bg-pink">
+    <v-row class="ma-5"> <h1>Velkommen!</h1></v-row>
+    <v-row class="ma-5">
+      <v-col>
+        Jeg heter Emily :) <br /><br />
+        Jeg kommer fra Molde, bor i Trondheim og studerer Datateknologi ved NTNU
+        med retning Databaser og søk.<br /><br />
+        Ved siden av studiene jobber jeg blant annet for Ada NTNU, et prosjekt
+        med fokus på å øke kvinneandelen som fullfører enkelte teknologistudier
+        ved NTNU. <br />
+        <br />
+        På fritiden liker jeg å se fotball, strikking og annet håndarbeid,
+        styrketrening og friluftsliv.<br /><br />
+        Jeg har laget en hjemmeside for å bli bedre kjent med frontend og få
+        litt erfaring med rammeverket Vue.
+      </v-col>
+      <v-col><v-img src="../assets/D0FDCD3D.jpg" class="ml-5"></v-img></v-col
+    ></v-row>
+  </v-container>
+</template>
 
-<script>
-export default {
-  name: "AboutMeComponent",
-};
-</script>
-
-<style scoped></style>
+<script setup lang="ts"></script>
