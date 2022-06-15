dbde8bb8bf661141ce510cb0cd1383963c6cd570
Emily Blakseth <ekblakse@stud.ntnu.no>
2022-06-15 20:45:55 +0200

    Update for better scalability on mobile devices

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <v-app class="bg-pink-lighten-4">
-    <v-app-bar color="pink"
-      ><v-spacer />
-      <v-btn @click="router.push('/')">Hjem</v-btn>
-      <v-btn @click="router.push('experience')">Erfaring</v-btn>
-      <v-btn @click="router.push('projects')">Prosjekter</v-btn>
-      <v-btn @click="router.push('contact')">Kontakt meg</v-btn></v-app-bar
+    <v-app-bar color="pink" max-width="500">
+      <v-spacer />
+      <v-slide-group multiple show-arrows>
+        <v-slide-item :key="1" class="ma-5">
+          <v-btn @click="router.push('/')">Hjem</v-btn>
+        </v-slide-item>
+        <v-slide-item :key="2">
+          <v-btn @click="router.push('experience')">Erfaring</v-btn>
+        </v-slide-item>
+        <v-slide-item :key="3">
+          <v-btn @click="router.push('projects')">Prosjekter</v-btn>
+        </v-slide-item>
+        <v-slide-item :key="4">
+          <v-btn @click="router.push('contact')">Kontakt meg</v-btn>
+        </v-slide-item>
+      </v-slide-group></v-app-bar
     ><v-main> <router-view /> </v-main>
     <v-footer class="bg-pink-lighten-4 text-center"
       ><v-spacer /> Juni 2022 <v-spacer />
@@ -28,5 +38,6 @@ h1 {
   color: #30051b;
   font-size: 50px;
   font-family: "Anton";
+  text-size-adjust: 80%;
 }
 </style>
